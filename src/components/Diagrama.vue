d1af238027b50b98c618e50601722ede9538c048
Unknown <begarzonf@unal.edu.co>
2020-02-15 23:04:38 -0500

    Prueba Completa
    Prueba Completa

--- src/components/Diagrama.vue
+++ src/components/Diagrama.vue
@@ -1,90 +1,97 @@
 <template>
-    <div class="Fondo">
-        <div class="Fondo2" :style="{'height':(numero*20)+'%'}" ></div>
-        <div v-for="items in cadena" :key="items.id" class="Eje">
-            {{obteneritems(items.string)}}
-            <div :class="{'eje1':!T,'eje2':T}">
-                <h1>E{{items.id+1}}</h1>
+  <div class="Fondo">
+    <div class="Fondo2" :style="{'height':(272*(numero-1))+'px'}"></div>
+    <div v-for="items in cadena" :key="items.id" class="Eje">
+      {{obteneritems(items.string)}}
+      <div :class="{'eje1':!T,'eje2':T}">
+        <h1>E{{items.id+1}}</h1>
+      </div>
+      <div :class="{'bar1':!T,'bar2':T}">
+        <div style="display: flex">
+          <div v-for="puntos in arrPuntos" :key="puntos" :style="'margin:auto'">
+            {{sumarpuntos()}}
+            <div class="circulo">
+              <p style="transform: translateY(50%)">P{{puntos1}}</p>
             </div>
-            <div :class="{'bar1':!T,'bar2':T}">
-                <div v-for="numero in arrllantas" :key="numero" :style="'margin:auto'">
-                    <div :class="{'llanta':!X,'llanta2':X}">
-                        <h1>{{numero}}</h1>
-                    </div>
-                    <div class="circulo">
-                        <p>P{{numero}}</p>
-                    </div>
-                    <div class="cuadrado">
-                    </div>
-                    -->
-                </div>
-            </div>
-
+            <div :class="{'cuadrado':!D,'cuadrado2':D}"></div>
+          </div>
         </div>
+        <div style="display: flex">
+          <div v-for="numero in arrllantas" :key="numero" :style="'margin:auto'">
+            {{sumarllantas()}}
+            <div :class="{'llanta':!X,'llanta2':X}">
+              <h1>{{llantas1}}</h1>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+    {{llantasof()}}{{puntosof()}}
+  </div>
 </template>
 
 <script>
-import {mapMutations} from 'vuex';
-import '@/sass/styles.sass'
+import { mapMutations, mapState } from "vuex";
+import "@/sass/styles.sass";
 export default {
-    data(){
-        return{
-            U: false,
-            D: false,
-            T: false,
-            X: false,
-            llantas: 1,
-            Papoyo: 1,
-            arrllantas: [],
-            arrPuntos: [],
-            numllantas: 0,
-            contador: 1,
-
-        }
+  data() {
+    return {
+      U: false,
+      D: false,
+      T: false,
+      X: false,
+      llantas: 1,
+      Papoyo: 1,
+      arrllantas: [],
+      arrPuntos: [],
+      numllantas: 0,
+      contador: 1
+    };
+  },
+  name: "Diagrama",
+  props: ["cadena", "numero"],
+  computed: {
+    ...mapState(["llantas1", "puntos1"])
+  },
+  methods: {
+    ...mapMutations(["sumarllantas", "sumarpuntos", "llantasof", "puntosof"]),
+    prueba() {
+      console.log("asd");
     },
-    name: 'Diagrama',
-    props: ['cadena','numero'],
-    methods:{
-        ...mapMutations(['obteneritem']),
-        prueba(){
-            console.log("asd")
-        },
-        obteneritems(items){
-            var a= new String(items)
-            this.arrPuntos=[];
-            this.arrllantas=[];
-            this.U= false;
-            this.D= false;
-            this.T= false;
-            this.X= false;
-            this.llantas=1;
-            this.Papoyo=1;
-            for(var i=0;i<a.length;i++){
-                if(items[i]=="T"){
-                    this.T=true;
-                }
-                if(items[i]=="D"){
-                    this.D=true;
-                    this.llantas=2;
-                }
-                if(items[i]=="U"){
-                    this.U=true;
-                    this.Papoyo=2;
-                }
-                if(items[i]=="X"){
-                    this.X=true;
-                }
-            }
-            this.numllantas=this.llantas*this.Papoyo;
-            for(var j=1;j<=this.numllantas;j++){
-                this.arrllantas.push(j);
-            }
-            for(var k=1;k<=this.Papoyo;k++){
-                this.arrPuntos.push(k);
-            }
+    obteneritems(items) {
+      var a = new String(items);
+      this.arrPuntos = [];
+      this.arrllantas = [];
+      this.U = false;
+      this.D = false;
+      this.T = false;
+      this.X = false;
+      this.llantas = 1;
+      this.Papoyo = 1;
+      for (var i = 0; i < a.length; i++) {
+        if (items[i] == "T") {
+          this.T = true;
         }
-
+        if (items[i] == "D") {
+          this.D = true;
+          this.llantas = 2;
+        }
+        if (items[i] == "U") {
+          this.U = true;
+          this.Papoyo = 2;
+        }
+        if (items[i] == "X") {
+          this.X = true;
+        }
+      }
+      this.numllantas = this.llantas * this.Papoyo;
+      for (var j = 1; j <= this.numllantas; j++) {
+        this.arrllantas.push(j);
+      }
+      for (var k = 1; k <= this.Papoyo; k++) {
+        this.arrPuntos.push(k);
+      }
     }
-}
+  }
+};
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    Lista:[
+    Lista: [
       '2-UDLR-XT',
       '2-UXL-L',
       '2-T-T',
@@ -15,52 +15,21 @@ export default new Vuex.Store({
       '4-DL-DL-DL-DL',
       '6-DLR-DLR-DT-DT-DLR-DLR'
     ],
-    U: false,
-    D: false,
-    T: false,
-    X: false,
-    llantas: 1,
-    Papoyo: 1,
-    arrllantas: [],
-    numllantas: 0,
-    contador: 0,
+    llantas1: 0,
+    puntos1: 0,
   },
   mutations: {
-    obteneritem(state,n){
-      console.log(n)
-      console.log("estoy en store")
-      var a= new String(n)
-      console.log(a)
-      state.arrllantas=[];
-      state.U= false;
-      state.D= false;
-      state.T= false;
-      state.X= false;
-      state.llantas=1;
-      state.Papoyo=1;
-      for(var i=0;i<a.length;i++){
-          console.log(n[i])
-          if(n[i]=="T"){
-              state.T=true;
-          }
-          if(n[i]=="D"){
-              state.D=true;
-              state.llantas=2;
-              console.log(state.D)
-          }
-          if(n[i]=="U"){
-              state.U=true;
-              state.Papoyo=2;
-          }
-          if(n[i]=="X"){
-              state.X=true;
-          }
-      }
-      state.numllantas=state.llantas*state.Papoyo;
-      for(var j=1;j<=state.numllantas;j++){
-          state.arrllantas.push(j);
-      }
-  }
+    sumarllantas(state) {
+      state.llantas1++;
+    }, sumarpuntos(state) {
+      state.puntos1++;
+    },
+    llantasof(state) {
+      state.llantas1=0;
+    },
+    puntosof(state) {
+      state.puntos1=0;
+    }
   },
   actions: {
   },
